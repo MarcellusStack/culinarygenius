e3c0f22739f4d0d45499f1551f0c13b8410a7141
MarcellusStack <marcellusstack@gmail.com>
2024-05-26 19:21:13 +0200

    setup posthog

--- app/_components/providers.tsx
+++ app/_components/providers.tsx
@@ -8,17 +8,29 @@ import { ModalsProvider } from "@mantine/modals";
 import { Notifications } from "@mantine/notifications";
 import { createTheme, DEFAULT_THEME, mergeMantineTheme } from "@mantine/core";
 
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
 const themeOverride = createTheme({
   primaryColor: "green",
 });
 
 export const theme = mergeMantineTheme(DEFAULT_THEME, themeOverride);
 
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    debug: process.env.NODE_ENV === "development",
+  });
+}
+
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <MantineProvider theme={theme}>
-      <Notifications />
-      <ModalsProvider>{children}</ModalsProvider>
-    </MantineProvider>
+    <PostHogProvider client={posthog}>
+      <MantineProvider theme={theme}>
+        <Notifications />
+        <ModalsProvider>{children}</ModalsProvider>
+      </MantineProvider>
+    </PostHogProvider>
   );
 };
